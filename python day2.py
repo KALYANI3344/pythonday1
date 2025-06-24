39af2dd5f54b34e418b55d0051a52e482c0b5b9e
ABBIREDDY LAKSHMI KALYANI <akalyani3344@gmail.com>
2025-06-24 18:57:40 +0530

    Add files via upload
    types of operators

---
+++ python day2.py
@@ -0,0 +1,31 @@
+"""
+An arthimetic operator is used to perform some mathematical/arthimetic
+the operators are +,-,*,/,//,%,**
+note:input()is a function which takes input from user
+"""
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print("the addition",a+b)
+print("the substration",a-b)
+print("the multiplication",a*b)
+print("the division",a/b)#which returns the quotient
+print ("theffloordivision ",a//b)#which returns the integeral quotient part in a division 
+print("the modulardivision",a%b)#which returns thd remaider part before the decimal 
+print("the power",a**b)#which returns the power
+
+
+"""
+realtional/comparision operators are used to compare the values
+and return the boolean they are
+>,<, >=,,=, == !=
+"""
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print("the greater value",a>b)
+print("the lesser value",a<b)
+print("the greather than or equals to value",a>=b)
+print("the less than or equals too value",a<b)
+print("the equals too value",a==b)
+print("the not equals too values",a!=b)
+
+
